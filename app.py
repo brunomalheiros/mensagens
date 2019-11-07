5b2f11be231c4b9ad57720d33815e9fa517344b8
Bruno Malheiros <brunopma10@gmail.com>
2019-11-07 08:38:59 -0300

    midificando app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return "Olá mundo!"
+def index():
+    return render_template('index.html')
+
+@app.route('/formulario')
+def formulario():
+    return render_template('formulario.html')
 
 if __name__ == '__main__':
     app.run()
